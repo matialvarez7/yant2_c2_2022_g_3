be1440c6034ac4d736f1a9377d683a8d5fbe1280
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-10-24 22:08:45 -0300

    se agregaron el router al main y modificaciones en el app.vue

--- yant2_c2_2022_g_3/src/main.js
+++ yant2_c2_2022_g_3/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import bootstrap from "./bootstrap/css/bootstrap.css";
 
 import './assets/main.css'
 
+const routes = [
+  {}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+Vue.use(VueRouter)
+
 new Vue({
+  router,
   render: (h) => h(App)
 }).$mount('#app')
