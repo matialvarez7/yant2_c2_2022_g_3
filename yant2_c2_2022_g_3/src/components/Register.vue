06c987dda6a72a5979cf06772fb50050a38946cb
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-11-07 16:20:44 -0300

    agregado componente register

---
+++ yant2_c2_2022_g_3/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+<!--class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register"-->
+    <form>
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+        <div class="row mb-4">
+            <div class="col">
+                <div class="form-outline">
+                    <input type="text" id="form3Example1" class="form-control" />
+                        <label class="form-label" for="form3Example1">First name</label>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-outline">
+                    <input type="text" id="form3Example2" class="form-control" />
+                        <label class="form-label" for="form3Example2">Last name</label>
+                </div>
+            </div>
+        </div>
+
+  <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="form3Example3" class="form-control" />
+                        <label class="form-label" for="form3Example3">Email address</label>
+                </div>
+
+  <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="form3Example4" class="form-control" />
+                        <label class="form-label" for="form3Example4">Password</label>
+                </div>
+
+  <!-- Checkbox -->
+                <div class="form-check d-flex justify-content-center mb-4">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+                        <label class="form-check-label" for="form2Example33">
+                            Subscribe to our newsletter
+                        </label>
+                </div>
+
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
+    </form>
+</div>
+</template>
